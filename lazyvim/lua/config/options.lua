05f6b257061c83e9155f05cc1a196be3c54443db
qiangxianfei <qiangxianfei@hengshi.com>
2026-04-20 10:03:32 +0800

    update 20260420

--- lazyvim/lua/config/options.lua
+++ lazyvim/lua/config/options.lua
@@ -1,16 +1,16 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+-- 放弃local opt = vim.opt 是因为有vim/_options.lua: Unkonw option 'g' 的报错
 
-local opt = vim.opt
-opt.relativenumber = false
+vim.opt.relativenumber = false
+vim.opt.expandtab = true
 -- 默认缩进4
-opt.expandtab = true
-opt.tabstop = 4
-opt.shiftwidth = 4
-opt.softtabstop = 4
-opt.guifont = "Fira Code:h14"
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.guifont = "Fira Code:h14"
 
 -- python
-opt.g.lazyvim_python_lsp = "pyright"
-opt.g.lazyvim_python_ruff = "ruff"
+vim.g.lazyvim_python_lsp = "pyright"
+vim.g.lazyvim_python_ruff = "ruff"
